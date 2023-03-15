322c5e7ce0e21f289be6c66cd0e5f11f43674941
Komal Singh <komalsingh54@gmail.com>
2023-03-15 22:03:41 +0000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,18 +1,7 @@
 const withPWA = require('next-pwa');
-const isGithubActions = process.env.GITHUB_ACTIONS || false
-let assetPrefix = ''
-let basePath = '/'
 
-if (isGithubActions) {
-  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
-
-  assetPrefix = `/${repo}/`
-  basePath = `/${repo}`
-}
 
 module.exports = /* withPWA( */{
-  assetPrefix: assetPrefix,
-  basePath: basePath,
   env: {
     NEXT_PUBLIC_RESUME_LINK: '1Y5ttDgKB_Q3NVjPe5lzKZxYOrP7nEX5v',
   },
